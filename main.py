dfafbdc55c8933e12ccd3443e4d001a7d3eaad3f
Sabas Gonzalez <sabasgt98@gmail.com>
2020-09-14 21:38:23 -0400

    Movimiento primitivo de piezas

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Juego de Checkers
+from pandas.io.sql import SQLAlchemyRequired
 import pygame
-from checkers.constants import * # Podemos hacer esto porque añadimos el archivo __init__.py a la carpeta, haciendo que Python reconozca todo como un paquete
+from checkers.constants import WIDTH, HEIGHT, SQUARE_SIZE # Podemos hacer esto porque añadimos el archivo __init__.py a la carpeta, haciendo que Python reconozca todo como un paquete
 from checkers.board import Board
 
 # Constantes del Render Engine
@@ -8,6 +9,15 @@ FPS = 60
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT)) # Tamaño de la pantalla
 pygame.display.set_caption("Checkers") # Nombre de la ventana del juego
+
+
+def get_row_col_from_mouse(pos):
+    x, y = pos
+    row = y // SQUARE_SIZE
+    col = x // SQUARE_SIZE
+
+    return row, col
+
 
 def main():
     run = True
@@ -22,7 +32,10 @@ def main():
                 run = False
 
             if event.type == pygame.MOUSEBUTTONDOWN:
-                pass
+                pos = pygame.mouse.get_pos()
+                row, col = get_row_col_from_mouse(pos)
+                piece = board.get_piece(row, col)
+                board.move(piece, 4, 3)
         
         board.draw(WIN)
         pygame.display.update()
